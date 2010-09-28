e2be383879cc62ee12b5733603e1fd6efa2d423e
ben7th <ben7th@sina.com>
2010-09-28 17:07:40 +0800

    修改 unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,34 @@
+# worker 数量
+worker_processes 3
+
+# 日志位置
+stderr_path("/web/2010/logs/unicorn-mindmap-image-cache-error.log")
+stdout_path("/web/2010/logs/unicorn-mindmap-image-cache.log")
+
+# 加载 超时设置 监听
+preload_app true
+timeout 30
+listen '/web/2010/sockets/unicorn-mindmap-image-cache.sock', :backlog => 2048
+
+pid_file_name = "/web/2010/pids/unicorn-mindmap-image-cache.pid"
+pid pid_file_name
+
+# REE GC
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
+
+before_fork do |server, worker|
+  old_pid = pid_file_name + '.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # ...
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
